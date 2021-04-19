8c0194d5cede0a4a681d2a2e90a8c1f087a96a62
@hexlet/cli <support@hexlet.io>
2021-04-19 05:57:16 +0000

    @hexlet/cli: submit

--- exercises/unit-testing/__tests__/index.test.js
+++ exercises/unit-testing/__tests__/index.test.js
@@ -4,6 +4,26 @@ test('main', () => {
   const result = Object.assign(target, src);
 
   // BEGIN
-  expect(result).toEqual({k: 'v', a: 'a', b: 'b'})
+  expect(result).toEqual({ k: 'v', a: 'a', b: 'b' });
   // END
+});
+
+test('that Object.assign return ref to target', () => {
+  const src = { c: 'c', d: 'd' };
+  const target = { a: 'a', b: 'b' };
+  const result = Object.assign(target, src);
+
+  expect(result).toBe(target);
+});
+
+test('throw error when Object.assign overwrite readonly prop', () => {
+  const src = { a: 'a2', b: 'b2' };
+  const target = { a: 'a' };
+
+  Object.defineProperty(target, 'b', {
+    value: 'b',
+    writable: false,
+  });
+
+  expect(() => Object.assign(target, src)).toThrow();
 });
