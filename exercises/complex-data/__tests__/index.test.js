1b6a7c6475ddb816ddd077ff67072bcfe7172e4c
@hexlet/cli <support@hexlet.io>
2021-04-22 10:52:40 +0000

    @hexlet/cli: submit

--- exercises/complex-data/__tests__/index.test.js
+++ exercises/complex-data/__tests__/index.test.js
@@ -12,4 +12,12 @@ test('complex data', () => {
     account: expect.any(String),
   });
 });
+
+test('uniq', () => {
+  const transaction1 = faker.helpers.createTransaction();
+  const transaction2 = faker.helpers.createTransaction();
+
+  expect(transaction1).not.toMatchObject(transaction2);
+  expect(transaction1).not.toBe(transaction2);
+});
 // END
