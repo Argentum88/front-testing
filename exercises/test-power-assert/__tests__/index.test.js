036d9ce04f70d6c3ce34928efe0fff044689e754
@hexlet/cli <support@hexlet.io>
2021-05-15 07:01:46 +0000

    @hexlet/cli: submit

--- exercises/test-power-assert/__tests__/index.test.js
+++ exercises/test-power-assert/__tests__/index.test.js
@@ -3,6 +3,7 @@ const { flattenDepth } = require('lodash');
 
 // BEGIN
 const array = [1, [2, [3, [4]], 5]];
+
 assert.deepEqual(flattenDepth(array, 1), [1, 2, [3, [4]], 5]);
 assert.deepEqual(flattenDepth(array, 2), [1, 2, 3, [4], 5]);
 assert.deepEqual(flattenDepth(array, 3), [1, 2, 3, 4, 5]);
