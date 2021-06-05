17042091777313bbb37fd80ba6a277914073e2a2
@hexlet/cli <support@hexlet.io>
2021-06-05 10:06:04 +0000

    @hexlet/cli: submit

--- exercises/e2e-best-practice/__mocks__/handlers.js
+++ exercises/e2e-best-practice/__mocks__/handlers.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 let tasks = [];
 export default [
-  rest.get('/tasks', (req, res, ctx) => res(ctx.status(200), ctx.json([]))),
+  rest.get('/tasks', (req, res, ctx) => res(ctx.status(200), ctx.json(tasks))),
 
   rest.post('/tasks', (req, res, ctx) => {
     const id = _.uniqueId();
@@ -11,7 +11,7 @@ export default [
     tasks = [...tasks, task];
     return res(
       ctx.status(201),
-      ctx.json([]),
+      ctx.json(task),
     );
   }),
 
--- exercises/e2e-best-practice/__tests__/test.js
+++ exercises/e2e-best-practice/__tests__/test.js
@@ -8,13 +8,12 @@ test('see button Add', async () => {
   await expect(page).toMatchElement('input[value=Add]');
 });
 
-test('can add tasks', async () => {
+test('can add task and delete it', async () => {
   await expect(page).toFillForm('form', { text: 'тыц' });
   await page.click('[data-testid=add-task-button]');
   await expect(page).toMatch('тыц');
-});
 
-test('see task again', async () => {
-  await expect(page).toMatch('тыц');
+  await page.click('[data-testid=remove-task-1]');
+  await expect(page).not.toMatch('тыц');
 });
 // END
