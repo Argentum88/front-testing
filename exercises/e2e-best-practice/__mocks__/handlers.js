b6249fcc9d03a9df01b9d2a28d973c14100dbc0a
@hexlet/cli <support@hexlet.io>
2021-06-05 03:29:21 +0000

    @hexlet/cli: submit

--- exercises/e2e-best-practice/__mocks__/handlers.js
+++ exercises/e2e-best-practice/__mocks__/handlers.js
@@ -3,12 +3,7 @@ import _ from 'lodash';
 
 let tasks = [];
 export default [
-  rest.get('/tasks', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json(tasks),
-    );
-  }),
+  rest.get('/tasks', (req, res, ctx) => res(ctx.status(200), ctx.json(tasks))),
 
   rest.post('/tasks', (req, res, ctx) => {
     const id = _.uniqueId();
