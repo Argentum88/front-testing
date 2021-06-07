e3e8a8bd75c91a2df698ba92efd2eb99231faa50
@hexlet/cli <support@hexlet.io>
2021-06-07 19:29:17 +0000

    @hexlet/cli: submit

--- exercises/testing-library-best-practice/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-
-import TodoBox from './TodoBox.jsx';
-
-ReactDOM.render(
-  <TodoBox />,
-  document.getElementById('container'),
-);
