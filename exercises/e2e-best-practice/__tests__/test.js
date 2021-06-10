f8400b99027bfe1b2411bfdbff3c5d3ba252411d
@hexlet/cli <support@hexlet.io>
2021-06-10 21:12:15 +0000

    @hexlet/cli: submit

--- exercises/e2e-best-practice/__tests__/test.js
+++ exercises/e2e-best-practice/__tests__/test.js
@@ -4,17 +4,23 @@ beforeEach(async () => {
   await page.goto('http://0.0.0.0:8080');
 });
 
-test('see button Add', async () => {
-  await expect(page).toMatchElement('input[value=Add]');
+test('see main page', async () => {
+  await expect(page).toMatchElement('[data-testid="task-name-input"]');
+  await expect(page).toMatchElement('[data-testid="add-task-button"]');
 });
 
 test('can add task and delete it', async () => {
-  const text = 'тыц';
-  await expect(page).toFillForm('form', { text });
+  const text1 = 'text1';
+  const text2 = 'text2';
+  await expect(page).toFillForm('form', { text: text1 });
   await page.click('[data-testid=add-task-button]');
-  await expect(page).toMatch(text);
+  await expect(page).toFillForm('form', { text: text2 });
+  await page.click('[data-testid=add-task-button]');
+  await expect(page).toMatch(text1);
+  await expect(page).toMatch(text2);
 
   await page.click('[data-testid=remove-task-1]');
-  await expect(page).not.toMatch(text);
+  await expect(page).toMatch(text2);
+  await expect(page).not.toMatch(text1);
 });
 // END
